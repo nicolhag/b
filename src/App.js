d826e3f443630d67c755a6ec0cc51676f6aa07d2
Nicolai Hagen <nicolhag@ifi.uio.no>
2018-12-24 12:09:55 +0100

    added some animations

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// <img src={logo} className="App-logo" alt="logo" />
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Her kommer en hyggelig bryllupsside!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>
+            Morten og Lise<br />gifter seg
+          </h1>
+          <img class="bilde" src="mortenoglise.jpg" height="150px" width="150px" />
+          <h1>
+            7.9.2019
+          </h1>
         </header>
+
       </div>
     );
   }
